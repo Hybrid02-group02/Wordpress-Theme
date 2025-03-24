8d95e4736a5f8ed05aa83df936a1592e5585b344
HANEUL <127837988+hanulls@users.noreply.github.com>
2025-03-24 11:13:00 +0900

    Update image2.php

--- patterns/image2.php
+++ patterns/image2.php
@@ -2,7 +2,7 @@
 return array(
     'title'       => 'image2',     // 패턴 이름(파일 이름과 비슷하거나 동일하게 추천)
     'description' => 'A custom pattern with title and 2 image.', // 패턴 설명
-    'categories'  => array('test'), // 원하는 카테고리 추가
+    'categories'  => array('basic'), // 원하는 카테고리 추가
     'content'     =>                // 내용(html 형식 작성)
     '
     <!-- wp:heading {"level":4} -->
